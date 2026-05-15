1a4b09006a8b9c963fa7d0b74278039e3cc38048
TEDDY TECH <kibetwycliffe093@gmail.com>
2026-05-15 11:07:54 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  BOT_NAME: 'WOLF TECH',
-  SESSION_PREFIX: 'WOLF',
-  AUTHOR: 'Silent Wolf',
-  DEPLOY_HOST: 'wolfxpair.xwolf.space',
-  PANEL_HOST: 'wolfxpair.xwolf.space',
-  GITHUB_URL: 'https://github.com/wolfxpair/wolfxpair',
+  BOT_NAME: 'TEDDY-XMD',
+  SESSION_PREFIX: 'TEDDY-XMD:~',
+  AUTHOR: 'TEDDY',
+  DEPLOY_HOST: 'https://teddyxmd1mini-1338329876db.herokuapp.com',
+  PANEL_HOST: 'https://teddyxmd1mini-1338329876db.herokuapp.com',
+  GITHUB_URL: 'https://github.com/Teddytech1/TEDDY-XMD',
   PORT: process.env.PORT || 3000,
 };
